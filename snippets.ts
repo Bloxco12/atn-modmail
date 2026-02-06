b7aed818cf883ec3f3233e99731768baab4018e4
Bloxco12 <118644302+Bloxco12@users.noreply.github.com>
2026-02-06 22:52:08 +0000

    Update snippets.ts

--- snippets.ts
+++ snippets.ts
@@ -1,1 +1,6 @@
-
+export default {
+	welcome:
+		"Dear {{usermention}}, thank you for contacting us today. Please allow me some time to look over your ticket, and then I will get back to you as soon as possible." /** Use the same syntax as the example to actually add a snippet, 
+  before : goes the name of the snippet, after the content of it. 
+  Note that if you need to write " remember to type a \ before it! The string can contain variables such as: {{userid}}, {{usermention}}, {{usertag}}. */,
+};
